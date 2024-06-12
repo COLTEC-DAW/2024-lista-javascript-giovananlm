ef452e463569eeb920288cdb317be2489765d5b0
Giovana Nassif Leonel Membrive <giovananlm@gmail.com>
2024-06-12 15:57:15 -0300

    Um programa diferente...

--- triangulo.js
+++ triangulo.js
@@ -39,4 +39,21 @@ function Palindromo(){
         console.log("Nao eh palindromo");
     }
 }
-Palindromo();
+//Palindromo();
+
+function Contando(){
+    for(let i = 0; i<=100; i += 1){
+        if(i % 3 == 0 && i % 5 == 0){
+            console.log("FizzBuzz");
+        }
+        else if(i % 3 == 0){
+            console.log("Fizz");
+        }
+        else if(i % 5 == 0){
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+Contando();
